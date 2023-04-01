124ba43c1d325daec96574a9e3268f7d7b1caac7
不安分的马努 <30515791+mr-pounds@users.noreply.github.com>
2023-04-01 15:15:33 +0800

    reset to acro design

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,12 @@
  * @Author       : zzz
  * @Date         : 2023-03-29 16:12:16
  * @LastEditors  : zzz
- * @LastEditTime : 2023-03-30 17:51:57
+ * @LastEditTime : 2023-03-30 21:50:24
  */
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { vitePluginForArco } from "@arco-plugins/vite-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vitePluginForArco({
-      // theme: "Gray",
-      // iconBox: "",
-      modifyVars: {
-        "primary-6": "#1DA57A",
-      },
-      style: true,
-      // varsInjectScope: []
-    }),
-    react(),
-  ],
+  plugins: [react()],
 });
